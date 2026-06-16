fa8d2672f790132bf7d907d8c545981c51f1a6de
tokareva-stacy <tokarevanyura@gmail.com>
2026-06-16 18:31:21 +0300

    deploy settings

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
+import htmlInject from 'vite-plugin-html-inject'
 
 export default defineConfig({
   root: 'src',
+  base: process.env.NODE_ENV === 'production' ? '/piano-teacher/' : '/',
   build: {
     outDir: '../dist'
-  }
+  },
+  plugins: [
+    htmlInject()
+  ]
 })
