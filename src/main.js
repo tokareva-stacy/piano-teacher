cd298b8f7800cd2578fa01329ff26381025c72e0
tokareva-stacy <tokarevanyura@gmail.com>
2026-06-16 16:56:16 +0300

    header

--- src/main.js
+++ src/main.js
@@ -1,3 +1,31 @@
 import './styles/main.scss';
-
 import './js/menu.js';
+
+const components = [
+  'header',
+  'hero',
+  'for-whom',
+  'about',
+  'journey',
+  'results',
+  'pricing',
+  'faq',
+  'cta',
+  'footer'
+];
+
+async function loadComponent(name) {
+  const response = await fetch(`/partials/${name}.html`);
+  return await response.text();
+}
+
+async function initApp() {
+  const app = document.getElementById('app');
+
+  for (const name of components) {
+    const html = await loadComponent(name);
+    app.insertAdjacentHTML('beforeend', html);
+  }
+}
+
+initApp();
